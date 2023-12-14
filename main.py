73f6e78f9fa05c14f14ae6245faa3dc833aa7f37
Pedro Vinícius de Almeida Silva Xavier <120400955+Pedrovinicius123@users.noreply.github.com>
2023-12-14 15:20:24 +0000

    Half complete!

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, url_for, flash
 
 app = Flask(__name__)
 
+components = ['Ana Clara', 'Pedro Vinícius', 'Enzo Lucattony',
+            'Angela', 'Ana Luiza', 'Sarah Beatriz', 'Diego', 'Jamilly']
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', integrants=components)
 
 @app.route('/login')
 def loginpage():
