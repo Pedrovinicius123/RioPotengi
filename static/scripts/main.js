995c0710d52d9f6b9df1d2e237afb6e9d14f25e6
Pedrovinicius123 <pvasx123@gmail.com>
2023-12-16 19:14:27 -0300

    Some adjustments

---
+++ static/scripts/main.js
@@ -0,0 +1,10 @@
+function toggleDeviceSize() {
+    
+    const body = document.body;
+    let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+    if (width > 600) {
+        
+    }
+
+}
